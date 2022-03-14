1c15598d438a6ef487a684b97db15a3512ea2051
dtsia20 <dtsia20@gmail.com>
2022-03-14 17:59:52 +0200

    day 2

---
+++ day 2/script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+
+//update count and total
+function updateSelectCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected'); 
+    const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf((seat)))
+    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//movie select click event
+movieSelect.addEventListener('change', e=>{
+    ticketPrice = +e.target.value;
+    updateSelectCount();
+})
+
+//seat click event
+container.addEventListener('click', e=> {
+    if(
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ){
+    e.target.classList.toggle("selected");
+    }
+
+    updateSelectCount();
+})
