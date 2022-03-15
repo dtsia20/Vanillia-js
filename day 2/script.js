abcf927b6726932734a668a0bdaa6980b90eb24e
dtsia20 <dtsia20@gmail.com>
2022-03-15 17:45:13 +0200

    day 2

--- day 2/script.js
+++ day 2/script.js
@@ -4,8 +4,16 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //update count and total
 function updateSelectCount() {
@@ -16,11 +24,33 @@ function updateSelectCount() {
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
+
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected');
+        }
+      });
+    }
+  
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  
+    if (selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+    }
+  }
 
 //movie select click event
 movieSelect.addEventListener('change', e=>{
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex,e.target.value)
     updateSelectCount();
 })
 
@@ -35,3 +65,6 @@ container.addEventListener('click', e=> {
 
     updateSelectCount();
 })
+
+// Initial count and total
+updateSelectCount();
