d3876916b4cb0b64421ef7ad108f3663a26cc946
dtsia20 <dtsia20@gmail.com>
2022-03-17 18:09:23 +0200

    dom array methods

---
+++ day 5/script.js
@@ -0,0 +1,51 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser =  {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    addData(newUser);    
+}
+
+// Add new obg to array
+function addData(obj) {
+    data.push(obj);
+
+    updateDOM();
+}
+
+// Update DOM
+function updateDOM (providedData = data) {
+    //clear DOM
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(user => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${user.name}</strong> ${formatMoney(user.money)}`;
+        main.appendChild(element);
+    });
+}
+// Format money 
+function formatMoney (number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
